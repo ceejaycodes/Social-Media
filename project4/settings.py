313e7e2bf18655e39e7d9feba1bbd805bec7149d
ceejaycodes <dreelceejay79@yahoo.com>
2023-07-11 14:57:11 +0100

    fix static

--- project4/settings.py
+++ project4/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 
 LOGIN_URL = '/login'
@@ -130,3 +130,10 @@ LOGIN_REDIRECT_URL='/'
 CRISPY_TEMPLATE_PACK = 'uni_form'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"static"), "templates"]
+
+# MEDIA_URL="static/media/"
+# MEDIA_ROOT=os.path.join(BASE_DIR,"static/media/")
